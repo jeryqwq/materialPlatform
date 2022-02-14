bd0f4b27511228444134bd8657a4648bec26ffe8
zcw001 <668304zcw@gmail.com>
2022-02-14 17:52:45 +0800

    feat 补充接口

--- src/server/index.js
+++ src/server/index.js
@@ -13,3 +13,33 @@ export const doMaterialAdd = (params) => {
 export const doMaterialUpdate = (params) => {
   return axios.post('/ssa/vis/material/update', params);
 };
+
+/**
+ * 物料下架
+ */
+export const doMaterialRemove = (params) => {
+  const { id } = params || {};
+  return axios.post(`/ssa/vis/material/remove?id=${id}`);
+};
+
+/**
+ * 物料列表查询（分页）
+ */
+export const doQueryPage = (params) => {
+  return axios.post('/ssa/vis/material/query/page', params);
+};
+
+/**
+ * 缩略图/物料文件上传
+ */
+export const doMaterialUpload = (params) => {
+  return axios.post('/ssa/vis/material/upload', params);
+};
+
+/**
+ * 物料版本列表查询（根据ID）
+ */
+export const doMaterialQueryVersions = (params) => {
+  const { id } = params || {};
+  return axios.post(`/ssa/vis/material/query/versions?id=${id}`);
+};
